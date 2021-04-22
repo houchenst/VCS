830065246de94b0de2cf892e4891739249042d14
unknown <trevormhouchens@gmail.com>
2021-04-22 00:00:48 -0400

    final commit

--- model.py
+++ model.py
@@ -27,17 +27,18 @@ class VariationalTransformer(nn.Module):
         self.logvar_layer = nn.Linear(self.embedding_size, self.z_dim)
 
         # initialize decoder parameters
-        self.initial_decoder_layer = nn.Linear(self.z_dim, 5*5*32)
+        self.initial_decoder_layer = nn.Linear(self.z_dim, 5*5*128)
         # (32,5,5) --> (16,25,25)
-        self.convt1 = nn.ConvTranspose2d(32, 16, 5, stride=5)
-        self.convt2 = nn.ConvTranspose2d(16,16, 5, stride=1, padding=2)
+        self.convt1 = nn.ConvTranspose2d(128, 64, 5, stride=5)
+        self.convt2 = nn.ConvTranspose2d(64,32, 5, stride=1, padding=2)
         # (16,25,25) --> (8,75,75)
-        self.convt3 = nn.ConvTranspose2d(16, 8, 7, stride=3, padding=2)
-        self.convt4 = nn.ConvTranspose2d(8,8,5,stride=1, padding=2)
+        self.convt3 = nn.ConvTranspose2d(32, 16, 7, stride=3, padding=2)
+        self.convt4 = nn.ConvTranspose2d(16,8,5,stride=1, padding=2)
         # (8,75,75) --> (4,150,150)
         self.convt5 = nn.ConvTranspose2d(8,4,4,stride=2, padding=1)
         # (4,150,150) --> (3,300,300) 3 channel 300x300 image
         self.convt6 = nn.ConvTranspose2d(4,3,4,stride=2, padding=1)
+        self.convt7 = nn.ConvTranspose2d(3,3,9,stride=1, padding=4)
 
         # parameter used to search latent space via gradient descent
         # this helps us find the attributes that describe an image
@@ -73,7 +74,7 @@ class VariationalTransformer(nn.Module):
 
     def decode(self, z):
         y = F.relu(self.initial_decoder_layer(z))
-        y = torch.reshape(y, (-1,32,5,5))
+        y = torch.reshape(y, (-1,128,5,5))
         # print(f"Original: {y.shape}")
         y = F.relu(self.convt1(y))
         # print(f"Convt1: {y.shape}")
@@ -101,7 +102,47 @@ class VariationalTransformer(nn.Module):
         # decode latent variables
         y = self.decode(z)
 
-        return y,mu,logvar
+        return y,mu,logvar,z
 
 
-# class Discriminator(nn.Module):
+class Discriminator(nn.Module):
+
+    def __init__(self, z_dim):
+        super().__init__()
+
+        self.z_dim = z_dim
+
+        # convolutional layers
+        # (300,300,3) --> (150,150,8)
+        self.conv1 = nn.Conv2d(3, 8, 5, stride=2, padding=2)
+        # (150,150,8) --> (75,75,16)
+        self.conv2 = nn.Conv2d(8, 16, 5, stride=2, padding=2)
+        # (75,75,16) --> (25,25,32)
+        self.conv3 = nn.Conv2d(16, 32, 7, stride=3, padding=3)
+        # (25,25,32) --> (5,5,64)
+        self.conv4 = nn.Conv2d(32, 64, 9, stride=5, padding=4)
+        # convert to z_dim vector
+        self.final_img_layer = nn.Linear(5*5*64, self.z_dim)
+
+        # combined z and image layers
+        self.dense1 = nn.Linear(2*self.z_dim, 64)
+        self.dense2 = nn.Linear(64, 32)
+        self.dense3 = nn.Linear(32,1)
+
+    def forward(self, z, img):
+        # pass image through convolutional layers
+        #(batch, 300, 300, 3) --> (batch, 3, 300, 300)
+        img_out = img.transpose(1,3)
+        img_out = F.relu(self.conv1(img_out))
+        img_out = F.relu(self.conv2(img_out))
+        img_out = F.relu(self.conv3(img_out))
+        img_out = F.relu(self.conv4(img_out))
+        img_out = torch.reshape(img_out, (-1, 5*5*64))
+        img_out = F.relu(self.final_img_layer(img_out))
+
+        # combine z and image features
+        joint_vec = torch.cat([img_out, z], dim=1)
+        joint_vec = F.relu(self.dense1(joint_vec))
+        joint_vec = F.relu(self.dense2(joint_vec))
+        
+        return torch.sigmoid(self.dense3(joint_vec))
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from comet_ml import Experiment
 from data import DeepFashionDataset
-from model import VariationalTransformer
+from model import VariationalTransformer, Discriminator
 from torch.utils.data import DataLoader
 from torch import nn, optim
 from torch.nn import functional as F
@@ -17,22 +17,41 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Set hyperparameters
 hyperparameters = {
-    "epochs": 2,
-    "batch_size": 32,
+    "epochs": 2000,
+    "batch_size": 64,
     "learning_rate": 1e-3,
     "max_attrs": 21, #similar to sequence length, max number of attributes to describe an outfit
-    "embedding_size": 256,
-    "latent_size": 256, #size of the learned latent space
-    "beta": 1e0, #balances KL and reconstruction term in loss function
+    "embedding_size": 1024,
+    "latent_size": 1024, #size of the learned latent space
+    "beta": 1e-5, #balances KL and reconstruction term in loss function
     # beta:
     # e1-10 - 1e-4
-    "recon_only": 0,
+    "recon_only": 0, #number of epochs at the start of training that should use reconstruction loss only
     "num_imgs": 8, #number of images to generate after each epoch
+    "recon": "both", #'gan' or 'mse' specifies the reconstruction loss to use
 }
+
+def gan_loss(true_images, gen_images, disc, zs):
+    '''
+    Used for reconstruction loss, approximates joint distribution P(z,y) for latent variable z and image y
+    '''
+    real_term = torch.mean(torch.log(disc(zs, true_images)+1e-8))
+    gen_term = torch.mean(torch.log(1. - disc(zs, gen_images)+1e-8))
+    return real_term + gen_term
+
+# mse takes disc and zs to match gan_loss signature
+def mse_loss(true_images, gen_images, disc, zs):
+    return F.mse_loss(true_images, gen_images)
+
+def both_loss(true_images, gen_images, disc, zs):
+    return mse_loss(true_images, gen_images, disc, zs) + gan_loss(true_images, gen_images, disc, zs)
+
+# specify reconstruction loss based on hyperparameters
+recon_loss = gan_loss if hyperparameters["recon"] == "gan" else (mse_loss if hyperparameters["recon"]=="mse" else both_loss)
 
 # evidence lower bound loss for VAEs
 # https://arxiv.org/pdf/1312.6114.pdf
-def elbo_loss(recon_y, y, mu, logvar, recon_only=False):
+def elbo_loss(recon_y, y, mu, logvar, zs, disc, recon_only=False):
     '''
     recon_y  - our network output, an image reconstructed from the input attributes
     y        - the ground truth image from our dataset
@@ -40,14 +59,14 @@ def elbo_loss(recon_y, y, mu, logvar, recon_only=False):
     logvar   - the log variance of our conditional distribution in latent space
     '''
     # reconstruction loss
-    mse = F.mse_loss(recon_y, y)
+    rl = recon_loss(y, recon_y, disc, zs)
 
     # KL Loss
     kll = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) * hyperparameters["beta"]
 
-    return mse + kll, mse, kll
+    return rl + kll, rl, kll
 
-def train_epoch(model, train_loader, optimizer, experiment, epoch, step, recon_only=False):
+def train_epoch(model, disc, train_loader, optimizer, disc_optimizer, experiment, epoch, step, recon_only=False):
     model.train()
     with experiment.train():
         epoch_loss = 0
@@ -56,32 +75,43 @@ def train_epoch(model, train_loader, optimizer, experiment, epoch, step, recon_o
             attrs = attrs.to(device)
             mask = mask.to(device)
             optimizer.zero_grad()
-            recon_y, mu, logvar = model(attrs, mask)
+            recon_y, mu, logvar, z = model(attrs, mask)
             if recon_only:
-                loss = F.mse_loss(recon_y,img)
+                loss = mse_loss(img,recon_y, disc, z)
             else:
-                loss, mse, kll = elbo_loss(recon_y, img, mu, logvar)
+                loss, rl, kll = elbo_loss(recon_y, img, mu, logvar, z, disc)
             epoch_loss += loss.item()
             loss.backward()
             optimizer.step()
+            if hyperparameters["recon"]=="gan" or hyperparameters["recon"]=="both":
+                # discriminator is better than generator to start
+                if epoch % 2 == 0:
+                    disc_optimizer.zero_grad()
+                    disc_loss = -1 * gan_loss(img, recon_y.detach(), disc, z.detach())
+                    disc_loss.backward()
+                    disc_optimizer.step()
+                    experiment.log_metric("Discriminator Loss", float(disc_loss.item()), step=step)
             if not recon_only:
-                experiment.log_metric("Reconstruction Loss", float(mse.item()), step=step)
+                experiment.log_metric(f"{hyperparameters['recon']} loss", float(rl.item()), step=step)
                 experiment.log_metric("KL Loss", float(kll.item()), step=step)
             experiment.log_metric("loss", float(loss), step=step)
             step+=1
         print(f"\tAverage train loss: {epoch_loss/len(train_loader)}")
     return step
 
-def train(model, train_loader, val_loader, optimizer, experiment, attr_list, val_every=5):
+def train(model, disc, train_loader, val_loader, optimizer, disc_optimizer, experiment, attr_list, save=False, val_every=5):
     step=0
     for e in range(hyperparameters["epochs"]):
         print(f"----------    EPOCH {e}    ----------")
-        step = train_epoch(model, train_loader, optimizer, experiment, e, step, recon_only=(e<hyperparameters["recon_only"]))
+        step = train_epoch(model, disc, train_loader, optimizer, disc_optimizer, experiment, e, step, recon_only=(e<hyperparameters["recon_only"]))
+        if save:
+            torch.save(model.state_dict(), './models/model.pt')
+            torch.save(disc.state_dict(), './models/disc.pt')
         if e%val_every == 0:
-            test(model, val_loader, experiment)
+            test(model, disc, val_loader, experiment)
         generate_image(model, val_loader, experiment, attr_list, num_imgs=hyperparameters["num_imgs"], epoch=e)
 
-def test(model, test_loader, experiment, val=True):
+def test(model, disc, test_loader, experiment, val=True):
     model.eval()
     with experiment.test(), torch.no_grad():
         test_loss=0
@@ -89,8 +119,8 @@ def test(model, test_loader, experiment, val=True):
             img = img.to(device)
             attrs = attrs.to(device)
             mask = mask.to(device)
-            recon_y, mu, logvar = model(attrs, mask)
-            loss, _, _= elbo_loss(recon_y, img, mu, logvar)
+            recon_y, mu, logvar,zs = model(attrs, mask)
+            loss, _, _= elbo_loss(recon_y, img, mu, logvar, zs, disc)
             test_loss += loss.item()
         if not val:
             print("TESTING:")
@@ -112,7 +142,7 @@ def generate_image(model, val_loader, experiment, attr_list, num_imgs=1, epoch=N
             img = img[:1].to(device)
             attrs = attrs[:1].to(device)
             mask = mask[:1].to(device)
-            recon_img, _, _ = model(attrs, mask)
+            recon_img, mu, logvar,z = model(attrs, mask)
             attr_names = [attr_list[int(attrs[0,i])] for i in range(attrs.shape[1]) if int(attrs[0,i]) != 0]
             img_name = "_".join(attr_names)
             if epoch is not None:
@@ -142,6 +172,8 @@ if __name__ == "__main__":
                         help="run testing loop")
     parser.add_argument("-g", "--gen", action="store_true", 
                         help="generate an image")
+    parser.add_argument("-S", "--smooth", action="store_true",
+                        help="generate 3 images to check the smoothness of the latent space")
     args = parser.parse_args()
 
     # Setup comet experiment
@@ -154,22 +186,41 @@ if __name__ == "__main__":
     test_set = DeepFashionDataset(args.data_root, set_type="test", max_attrs=hyperparameters["max_attrs"])
     attrs = train_set.attrs
     attr2num = train_set.attr2num
-    train_loader = DataLoader(train_set, batch_size=hyperparameters["batch_size"], shuffle=True, drop_last=True, num_workers=2)
-    val_loader = DataLoader(val_set, batch_size=hyperparameters["batch_size"], shuffle=True, drop_last=True)
-    test_loader = DataLoader(test_set, batch_size=hyperparameters["batch_size"], shuffle=True, drop_last=True)
+    train_loader = DataLoader(train_set, batch_size=hyperparameters["batch_size"], shuffle=True, drop_last=True, num_workers=0)
+    val_loader = DataLoader(val_set, batch_size=hyperparameters["batch_size"], shuffle=True, drop_last=True, num_workers=0)
+    test_loader = DataLoader(test_set, batch_size=hyperparameters["batch_size"], shuffle=True, drop_last=True, num_workers=0)
 
     # initialize model
     model = VariationalTransformer(hyperparameters["max_attrs"], len(attrs), hyperparameters["embedding_size"], hyperparameters["latent_size"]).to(device)
+    disc = Discriminator(hyperparameters["latent_size"]).to(device)
     optimizer = optim.Adam(model.parameters(), lr=hyperparameters["learning_rate"])
+    disc_optimizer = optim.Adam(disc.parameters(), lr=hyperparameters["learning_rate"])
 
     if args.load:
         model.load_state_dict(torch.load('./models/model.pt'))
+        disc.load_state_dict(torch.load('./models/disc.pt'))
     if args.train:
-        train(model, train_loader, val_loader, optimizer, experiment, attrs)
+        train(model, disc, train_loader, val_loader, optimizer, disc_optimizer, experiment, attrs, save=args.save)
     if args.test:
-        test(model, test_loader, experiment, val=False)
-    if args.save:
-        torch.save(model.state_dict(), './models/model.pt')
+        test(model, disc, test_loader, experiment,  val=False)
+    # save every epoch during training instead 
+    # if args.save:
+    #     torch.save(model.state_dict(), './models/model.pt')
+    #     torch.save(disc.state_dict(), './models/disc.pt')
     if args.gen:
         generate_image(model, val_loader, experiment, attrs, log=False, show=True, save=True)
+    if args.smooth:
+        model.eval()
+        with torch.no_grad():
+            z1 = torch.zeros((hyperparameters["latent_size"])).to(device)
+            z2 = 0.01*torch.ones((hyperparameters["latent_size"])).to(device)
+            z3 = 1.*torch.ones((hyperparameters["latent_size"])).to(device)
+            z4 = 10.*torch.ones((hyperparameters["latent_size"])).to(device)
+            vecs = torch.vstack([z1,z2,z3,z4]).to(device)
+            ys = model.decode(vecs)
+            experiment.log_image(ys[0].cpu().numpy(), "z_zero")
+            experiment.log_image(ys[1].cpu().numpy(), "z_hundredth")
+            experiment.log_image(ys[2].cpu().numpy(), "z_one")
+            experiment.log_image(ys[3].cpu().numpy(), "z_ten")
+
 
